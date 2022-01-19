38231a0d7a29b35a261286568d0adcbfd2707f7a
Dabu0319 <68059673+Dabu0319@users.noreply.github.com>
2022-01-19 14:30:43 +0800

    Test01_1/19/2022

---
+++ Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeatScroller theBS;
+
+
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
+
+    
+    
+    
+    
+    
+    public Text scoreText;
+    public Text multiText;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierTresholds;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
+    }
+
+    
+    
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+                
+                theMusic.Play();
+            }
+        }
+        
+    }
+
+    public void NoteHit()
+    {
+
+        Debug.Log("Hit on time");
+
+        //Lenth也就是最多可以加到多少层
+        if (currentMultiplier - 1 < multiplierTresholds.Length)
+        {
+            multiplierTracker++;
+            if (multiplierTresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+        
+
+
+        
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
+    }
+    
+    public void NoteMissed()
+    {
+        
+        Debug.Log("miss");
+        
+        //Miss后重新记录
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+    }
+    
+    
+}
---
+++ Assets/Rhythm Game Tutorial/Scripts/NoteObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                //gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                gameObject.SetActive(false);
+
+                //点击成功
+                //GameManager.instance.NoteHit();
+                if (transform.position.y > 0.25)
+                {
+                    
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+            
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator" && gameObject.activeSelf)
+        {
+            canBePressed = false;
+            
+            //失败
+            GameManager.instance.NoteMissed();
+            
+        }
+    }
+}
